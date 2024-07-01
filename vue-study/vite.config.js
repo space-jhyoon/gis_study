d0ed9f64253857cd7a059c5cd6d0601c83637dbb
jhyoon <space.jhyoon@gmail.com>
2024-07-01 15:38:55 +0900

    초기 세팅

--- vue-study/vite.config.js
+++ vue-study/vite.config.js
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/v
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      // "/gis_study/test": 'http://localhost:8080',
+      "/test": 'http://localhost:8080',
+    }
+  },
+  // base: '/gis_study'
 })
